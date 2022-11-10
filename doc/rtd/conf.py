88306523ae22dd1e33f8de6c49243f05a44b18a5
Brett Holman <brett.holman@canonical.com>
2022-11-10 09:12:06 -0700

    init - modules page needs some work

--- doc/rtd/conf.py
+++ doc/rtd/conf.py
@@ -65,7 +65,7 @@ show_authors = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
