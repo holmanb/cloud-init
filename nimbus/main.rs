0b30d0f23587f20bdb68ae8343850dadbc0b7e23
Brett Holman <brett.holman@canonical.com>
2023-11-07 03:57:37 +0200

    nimbus: Rust package POC
    Example of using rust with dependencies sourced from a local Debian
    package registry.
    
    Note: No Cargo.lock included since some Debian checksum are missing.

---
+++ nimbus/main.rs
@@ -0,0 +1,9 @@
+#![deny(warnings)]
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let body = reqwest::blocking::get("https://www.rust-lang.org")?.text()?;
+
+    println!("body = {:?}", body);
+
+    Ok(())
+}
