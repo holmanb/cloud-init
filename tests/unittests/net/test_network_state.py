0136cbd7f4879bd6eddfb1ba80b9fef857276012
Brett Holman <brett.holman@canonical.com>
2024-04-16 16:36:13 -0600

    move network_state test into correct file

--- tests/unittests/net/test_network_state.py
+++ tests/unittests/net/test_network_state.py
@@ -350,3 +350,31 @@ class TestNetworkStateHelperFunctions(CiTestCase):
         expected = 48
         prefix_value = network_state.ipv6_mask_to_net_prefix(netmask_value)
         assert prefix_value == expected
+
+
+class TestNetDns:
+    @mock.patch("cloudinit.net.network_state.get_interfaces_by_mac")
+    @mock.patch("cloudinit.net.get_interfaces_by_mac")
+    def test_system_mac_address_does_not_break_dns_parsing(
+        self, by_mac_state, by_mac_init
+    ):
+        by_mac_state.return_value = {"00:11:22:33:44:55": "foobar"}
+        by_mac_init.return_value = {"00:11:22:33:44:55": "foobar"}
+        state = network_state.parse_net_config_data(
+            yaml.safe_load(
+                """\
+version: 2
+ethernets:
+  eth:
+    match:
+      macaddress: '00:11:22:33:44:55'
+    addresses: [10.0.0.2/24]
+    gateway4: 10.0.0.1
+    nameservers:
+      addresses: [10.0.0.3]
+"""
+            )
+        )
+        assert (
+            "10.0.0.3" in next(state.iter_interfaces())["dns"]["nameservers"]
+        )
