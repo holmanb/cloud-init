ada46ee3a0eb4c2daa2a66c2a6833f39a5973956
Brett Holman <brett.holman@canonical.com>
2025-03-13 18:51:28 -0600

    update

--- tools/graph.py
+++ tools/graph.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+import time
+from copy import deepcopy
+
 import networkx as nx
 from pprint import pprint
 import pygraphviz
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 # TODO: note to future self
 # it's probably easier to just iterate over every combination of nodes
@@ -30,65 +34,115 @@ import matplotlib.pyplot as plt
 
 CONFLICTS = "red"
 ORDER_ATTR = "green"
-MEMBERSHIP_ATTRS = ["black", "gold", "grey66", "black"]
-CLOUD_INIT_UNITS = ["cloud-init.target", "cloud-init-local.service", "cloud-init-network.service", "cloud-config.service", "cloud-final.service", "cloud-init-main.service"]
+MEMBERSHIP_ATTRS = ["black", "gold", "grey66", "black", "darkblue"]
+CLOUD_INIT_UNITS = [
+        "cloud-init.target",
+        "cloud-init-local.service",
+        "cloud-init-network.service",
+        "cloud-config.service",
+        "cloud-final.service",
+        "cloud-init-main.service"
+]
 
 SHUTDOWN = "shutdown.target"
 DEFAULT = "graphical.target"
 
 order_edges = []
-membership_edges = []
+requirement_edges = []
 
-full_graph = nx.DiGraph(nx.nx_agraph.read_dot("./out/dot-oracular-cleaned.dot"))
+full_graph = nx.DiGraph(
+    nx.nx_agraph.read_dot("./out/dot-oracular-cleaned.dot")
+)
+order_graph_raw = nx.DiGraph(
+    nx.nx_agraph.read_dot("./out/dot-order-oracular-cleaned.dot")
+)
+require_graph = nx.DiGraph(
+    nx.nx_agraph.read_dot("./out/dot-require-oracular-cleaned.dot")
+)
 
-for source, dest, attr in full_graph.edges.data("color"):
-    if attr in CONFLICTS or source == SHUTDOWN or dest == SHUTDOWN:
+assert nx.is_directed_acyclic_graph(order_graph_raw)
+assert nx.has_path(order_graph_raw, source="cloud-init.target", target="cloud-config.service")
+assert nx.has_path(order_graph_raw, source="cloud-init-local.service", target="cloud-init-main.service")
+assert nx.has_path(order_graph_raw, source="cloud-init-network.service", target="cloud-init-main.service")
+assert nx.has_path(order_graph_raw, source="cloud-config.service", target="cloud-init-main.service")
+assert nx.has_path(order_graph_raw, source="cloud-final.service", target="cloud-init-main.service")
+assert nx.has_path(order_graph_raw, source="cloud-init.target", target="cloud-init-local.service")
+
+#for edge in full_graph.edges(data=True):
+#    src, dst, attr = edge
+#    if src == "cloud-init.target":
+#        print(src, dst, attr)
+
+
+for source, dest, attr in require_graph.edges.data("color"):
+    if attr == CONFLICTS or source == SHUTDOWN or dest == SHUTDOWN:
         continue
-    elif attr in ORDER_ATTR:
-        order_edges.append((source, dest))
     elif attr in MEMBERSHIP_ATTRS:
-        membership_edges.append((source, dest))
+        requirement_edges.append((source, dest))
+    else:
+        breakpoint()
+
+for source, dest, attr in order_graph_raw.edges.data("color"):
+    if source == SHUTDOWN or dest == SHUTDOWN:
+        continue
+    elif attr == ORDER_ATTR:
+        order_edges.append((source, dest))
+    else:
+        breakpoint()
 
 # order_graph contains all ordering relationships
 order_graph = full_graph.edge_subgraph(order_edges)
-assert nx.is_directed_acyclic_graph(order_graph)
 
-# membership_graph contains all membership relationships between nodes
-membership_graph = full_graph.edge_subgraph(membership_edges)
+# requirement_graph contains all membership relationships between nodes
+requirement_graph = require_graph.edge_subgraph(requirement_edges)
 
-boot_target_nodes = []
-for node in membership_graph.nodes:
-    if node not in order_graph:
-        print(f"node: {node} not in order_graph: {order_graph}")
-    if node in nx.descendants(membership_graph, DEFAULT):
-        boot_target_nodes.append(node)
+# get descendants of the default target from the requirement graph
+default_descendants = nx.descendants(requirement_graph, DEFAULT)
+non_descendants = requirement_graph.nodes - default_descendants
+boot_order = order_graph.subgraph(default_descendants)
+print("non-descendants:")
+pprint(non_descendants)
+
+
+# find neighbors of cloud-init units
+neighbors = [*CLOUD_INIT_UNITS]
+for unit in CLOUD_INIT_UNITS:
+    assert unit in boot_order.nodes
+    for neighbor in boot_order.neighbors(unit):
+        neighbors.append(neighbor)
+neighbors = list(set(neighbors))
+print("neighbors:")
+pprint(neighbors)
+
+paths_between_neighbors = set()
+start = time.time()
+print(f"getting simple paths for {neighbors}")
+for src in neighbors:
+    nodes = []
+    # get all paths via reachable nodes
+    for node in neighbors:
+        if nx.has_path(boot_order, source=src, target=node):
+            nodes.append(node)
+    print(f"getting simple paths for {src} -> {nodes}")
+    for path in nx.all_simple_paths(boot_order, source=src, target=nodes):
+        paths_between_neighbors.update(set(path))
+
+print(f"getting paths took {time.time() - start}s")
 breakpoint()
+paths_between_neighbors = list(set(paths_between_neighbors))
+paths_between_neighbors.append(DEFAULT)
+print("paths between neighbors:")
+pprint(paths_between_neighbors)
 
-# boot_target contains all nodes required in the boot target
-boot_target = order_graph.subgraph(boot_target_nodes)
+cloud_init_graph = boot_order.subgraph(paths_between_neighbors)
 
-found_cloud_init_units = []
-cloud_init_neighbors = []
-for node in boot_target.nodes:
-    if node in CLOUD_INIT_UNITS:
-        found_cloud_init_units.append(node)
-        for neighbor in boot_target.neighbors(node):
-            cloud_init_neighbors.append(neighbor)
-assert set(found_cloud_init_units) == set(CLOUD_INIT_UNITS)
-breakpoint()
+reversed= nx.reverse_view(cloud_init_graph)
+reduced = nx.transitive_reduction(reversed)
+if set(reduced.edges()) != set(reversed.edges()):
+    print("transitive reduction did nothing!")
+    breakpoint()
+graph = reversed
 
-# identify the _order_ of all nodes that are members of the boot transaction
-# identified by the boot target graphical.target
-member_order = order_graph.subgraph(boot_target.nodes)
-assert nx.is_directed_acyclic_graph(member_order)
-breakpoint()
-
-# identify only nodes that are descendants of graphical.target
-# graph = member_order.subgraph(nx.ancestors(member_order, DEFAULT))
-#nx.transitive_reduction(member_order)
-#graph = member_order
-
-graph = nx.reverse_view(member_order)
 
 # https://networkx.org/documentation/stable/auto_examples/graph/plot_dag_layout.html#sphx-glr-auto-examples-graph-plot-dag-layout-py
 for layer, nodes in enumerate(nx.topological_generations(graph)):
@@ -101,7 +155,8 @@ for layer, nodes in enumerate(nx.topological_generations(graph)):
 pos = nx.multipartite_layout(graph, subset_key="layer")
 
 fig, ax = plt.subplots()
-nx.draw_networkx(graph, pos=pos, ax=ax)
+nx.draw_networkx(graph, pos=pos, ax=ax, with_labels=False,) #arrowsize=1, linewidths=0.1 )
+nx.draw_networkx_labels(graph, pos=pos, ax=ax, verticalalignment="bottom", font_weight="normal")
 ax.set_title("DAG layout in topological order")
 fig.set_layout_engine(layout="constrained")
 plt.show()
