1ac7c3b3c06acd8c83807456deb3e48b58f0e5c6
Brett Holman <brett.holman@canonical.com>
2025-04-08 16:54:27 -0600

    partial: path updates but also some work on the tako modified ordering

--- tools/gather-graphs.sh
+++ tools/gather-graphs.sh
@@ -18,6 +18,9 @@ RESULTS=out
 # CLOUD_INIT is the cloud-init target
 CLOUD_INIT=cloud-init
 
+# TAKO_BINARY_PATH is the path of the pre-built tako binary
+TAKO_BINARY_PATH=$HOME/upstream/tako/tako
+
 # wait_for_target manually waits since cloud-init status --wait is insufficient:
 # 1) `lxc exec` fails when vm isn't booted yet
 # 2) `cloud-init status --wait` expects fs artifacts which don't exist when services
@@ -59,22 +62,22 @@ function wait_for_target(){
 function gather(){
     local INSTANCE=$1
     local OUT=$2
-    local FLAVOR=$3
+
+    mkdir -p $OUT
     # defaults to gathering for multi-user.target
-    lxc exec $INSTANCE -- systemd-analyze dot > $OUT/dot-$INSTANCE-$FLAVOR.dot 2>/dev/null
-    lxc exec $INSTANCE -- systemd-analyze dot --order > $OUT/dot-order-$INSTANCE-$FLAVOR.dot 2>/dev/null
-    lxc exec $INSTANCE -- systemd-analyze dot --require > $OUT/dot-require-$INSTANCE-$FLAVOR.dot 2>/dev/null
-    lxc exec $INSTANCE -- systemd-analyze critical-chain > $OUT/chain-$INSTANCE-$FLAVOR.txt
-    lxc exec $INSTANCE -- systemd-analyze critical-chain --fuzz=60> $OUT/chain-fuzz-$INSTANCE-$FLAVOR.txt
-    lxc exec $INSTANCE -- systemd-analyze > $OUT/analyze-$INSTANCE-$FLAVOR.txt
-    lxc exec $INSTANCE -- systemd-analyze blame > $OUT/blame-$INSTANCE-$FLAVOR.txt
-    lxc exec $INSTANCE -- systemd-analyze dump > $OUT/dump-$INSTANCE-$FLAVOR.txt
+    lxc exec $INSTANCE -- systemd-analyze dot > $OUT/dot-total.dot 2>/dev/null
+    lxc exec $INSTANCE -- systemd-analyze dot --order > $OUT/dot-order.dot 2>/dev/null
+    lxc exec $INSTANCE -- systemd-analyze dot --require > $OUT/dot-require.dot 2>/dev/null
+    lxc exec $INSTANCE -- systemd-analyze critical-chain > $OUT/chain.txt
+    lxc exec $INSTANCE -- systemd-analyze critical-chain --fuzz=60> $OUT/chain-fuzz.txt
+    lxc exec $INSTANCE -- systemd-analyze > $OUT/analyze.txt
+    lxc exec $INSTANCE -- systemd-analyze blame > $OUT/blame.txt
+    lxc exec $INSTANCE -- systemd-analyze dump > $OUT/dump.txt
 
     # remove the cache and reload the daemon
-    lxc exec $INSTANCE -- rm -rf /run/cloud-init/
-
-    lxc exec $INSTANCE -- time -o tmp systemctl daemon-reload
-    lxc exec $INSTANCE -- cat tmp > $OUT/uncached-reload-$INSTANCE-$FLAVOR.txt
+    #lxc exec $INSTANCE -- rm -rf /run/cloud-init/
+    #lxc exec $INSTANCE -- time -o tmp systemctl daemon-reload
+    #lxc exec $INSTANCE -- cat tmp > $OUT/uncached-reload.txt
 }
 
 function clean_rerun(){
@@ -88,7 +91,7 @@ function clean_rerun(){
     # gather cached data
     lxc start $INSTANCE
     wait_for_target $INSTANCE $WAIT_TARGET
-    gather $INSTANCE $OUT $FLAVOR
+    gather $INSTANCE "$OUT/$INSTANCE/$FLAVOR"
 }
 
 function gather_first_boot(){
@@ -98,7 +101,7 @@ function gather_first_boot(){
 
     # gather first-boot data
     wait_for_target $INSTANCE $CLOUD_INIT
-    gather $INSTANCE $OUT "first-boot"
+    gather $INSTANCE "$OUT/$INSTANCE/first-boot"
 
 }
 
@@ -140,6 +143,7 @@ function gather_no_ops(){
         lxc exec $INSTANCE -- rm -rf $DIR
     done
 }
+
 function gather_divide_conquer_disabled(){
     local ETC_SYSTEM=etc/systemd/system
     local LIB_SYSTEM=lib/systemd/system
@@ -186,9 +190,167 @@ function gather_divide_conquer_disabled(){
     clean_rerun $INSTANCE "divide-conquer-disabled" $GRAPHICAL
 
     # teardown
-    for DIR in /etc/systemd/system/cloud-init-local.service.d/ /etc/systemd/system/cloud-init-network.service.d/ /etc/systemd/system/cloud-config.service.d/ /etc/systemd/system/cloud-final.service.d/; do
-        lxc exec $INSTANCE -- rm -rf $DIR
+    lxc exec $INSTANCE -- rm /etc/cloud/cloud-init.disabled
+    for DIR in $LOCAL_D $NETWORK_D $CONFIG_D $FINAL_D; do
+        lxc exec $INSTANCE -- rm -rf /$ETC_SYSTEM/$DIR
     done
+    for DIR in $LOCAL_AFTER_D $NETWORK_AFTER_D $CONFIG_AFTER_D $FINAL_AFTER_D; do
+        lxc exec $INSTANCE -- rm -rf /$ETC_SYSTEM/$DIR
+    done
+    # create "after services"
+    lxc exec $INSTANCE -- rm -f /$LIB_SYSTEM/cloud-init-local-after.service
+    lxc exec $INSTANCE -- rm -f /$LIB_SYSTEM/cloud-init-network-after.service
+    lxc exec $INSTANCE -- rm -f /$LIB_SYSTEM/cloud-config-after.service
+    lxc exec $INSTANCE -- rm -f /$LIB_SYSTEM/cloud-final-after.service
+    lxc exec $INSTANCE -- rm -rf /$LIB_SYSTEM/$MAIN_D
+}
+
+function install_file(){
+    local TEMP_SERVICE=$(mktemp)
+    local INSTANCE="$1"
+    local FILE_NAME="$2"
+    local CONTENT="$3"
+    echo $CONTENT > $TEMP_SERVICE
+    lxc file push $TEMP_SERVICE $INSTANCE/$LIB_SYSTEM/$FILE_NAME
+}
+
+function gather_tako_disabled(){
+    local ETC_SYSTEM=etc/systemd/system
+    local LIB_SYSTEM=lib/systemd/system
+    local MAIN_D=cloud-init-main.service.d
+    local LOCAL_D="cloud-init-local.service.d"
+    local NETWORK_D="cloud-init-network.service.d"
+    local CONFIG_D="cloud-config.service.d"
+    local FINAL_D="cloud-final.service.d"
+    local LOCAL_AFTER_D="cloud-init-local-after.service.d"
+    local NETWORK_AFTER_D="cloud-init-network-after.service.d"
+    local CONFIG_AFTER_D="cloud-config-after.service.d"
+    local FINAL_AFTER_D="cloud-final-after.service.d"
+    local TAKO_INSTALLED_PATH="/usr/libexec/"
+    local PERSISTENT_TEMP="/usr/local/temp/"
+    local INSTANCE="$1"
+    local OVERRIDE_AFTER=$(mktemp)
+    local OVERRIDE_BEFORE=$(mktemp)
+    local OVERRIDE_MAIN=$(mktemp)
+# /usr/lib/systemd/system/cloud-init-main.service
+# systemd ordering resources
+# ==========================
+# https://systemd.io/NETWORK_ONLINE/
+# https://docs.cloud-init.io/en/latest/explanation/boot.html
+# https://www.freedesktop.org/wiki/Software/systemd/NetworkTarget/
+# https://www.freedesktop.org/software/systemd/man/latest/systemd.special.html
+# https://www.freedesktop.org/software/systemd/man/latest/systemd-remount-fs.service.html
+#[Unit]
+#Description=Cloud-init: Single Process
+#DefaultDependencies=no
+#
+#After=systemd-remount-fs.service
+#Before=cloud-init-local.service
+#Before=shutdown.target
+#Conflicts=shutdown.target
+#RequiresMountsFor=/var/lib/cloud
+#ConditionPathExists=!/etc/cloud/cloud-init.disabled
+#ConditionKernelCommandLine=!cloud-init=disabled
+#ConditionEnvironment=!KERNEL_CMDLINE=cloud-init=disabled
+#
+#[Service]
+#Type=notify
+#ExecStart=/usr/bin/cloud-init --all-stages
+#KillMode=process
+#TasksMax=infinity
+#TimeoutStartSec=infinity
+#
+## Output needs to appear in instance console output
+#StandardOutput=journal+console
+#
+#[Install]
+#WantedBy=cloud-init.target
+
+    #
+    # setup
+    #
+    # remove all services
+    lxc exec $INSTANCE -- mkdir -p $PERSISTENT_TEMP
+    lxc exec $INSTANCE -- mv /$LIB_SYSTEM/cloud-init-main.service $PERSISTENT_TEMP
+    lxc exec $INSTANCE -- mv /$LIB_SYSTEM/cloud-init-local.service $PERSISTENT_TEMP
+    lxc exec $INSTANCE -- mv /$LIB_SYSTEM/cloud-init-network.service $PERSISTENT_TEMP
+    lxc exec $INSTANCE -- mv /$LIB_SYSTEM/cloud-config.service $PERSISTENT_TEMP
+    lxc exec $INSTANCE -- mv /$LIB_SYSTEM/cloud-final.service $PERSISTENT_TEMP
+    #lxc exec $INSTANCE -- mv /$LIB_SYSTEM/cloud-config.target $PERSISTENT_TEMP
+    #lxc exec $INSTANCE -- mv /$LIB_SYSTEM/cloud-init.target $PERSISTENT_TEMP
+
+    local tako_daemon=$(cat << EOD
+[Unit]
+Description=tako daemon
+DefaultDependencies=no
+
+After=systemd-remount-fs.service
+Before=shutdown.target
+Conflicts=shutdown.target
+
+[Service]
+Environment="TAKO=/run/tako/"
+Type=simple
+ExecStartPre=mkdir -p /run/tako
+ExecStart=$TAKO_INSTALLED_PATH run
+
+# Output needs to appear in instance console output
+StandardOutput=kmsg
+
+[Install]
+WantedBy=cloud-init.target
+EOD
+)
+    local local_before_hv=$(cat << EOD
+[Unit]
+Description=tako client wrapper service: hv_kvp_daemon.service
+DefaultDependencies=no
+
+After=systemd-remount-fs.service
+Before=shutdown.target
+Conflicts=shutdown.target
+
+[Service]
+Environment="TAKO=/run/tako/"
+Type=simple
+ExecStartPre=mkdir -p /run/tako
+ExecStart=$TAKO_INSTALLED_PATH run
+
+# Output needs to appear in instance console output
+StandardOutput=kmsg
+
+[Install]
+WantedBy=cloud-init.target
+EOD
+)
+    install_file $INSTANCE tako-daemon.service $tako_daemon
+
+    # enable cloud-init
+    lxc exec $INSTANCE -- systemctl enable cloud-init.target
+
+    # install tako binary
+    lxc file push $TAKO_BINARY_PATH $INSTANCE/$TAKO_INSTALLED_PATH
+
+
+    #printf '[Unit]\nAfter=\n\n[Service]\nExecStart=\nExecStart=true\n' > $OVERRIDE_AFTER
+    #printf '[Unit]\nBefore=\n\n[Service]\nExecStart=\nExecStart=true\n' > $OVERRIDE_BEFORE
+    #lxc exec $INSTANCE -- touch /etc/cloud/cloud-init.disabled
+    #for DIR in $LOCAL_D $NETWORK_D $CONFIG_D $FINAL_D; do
+    #    lxc exec $INSTANCE -- mkdir -p /$ETC_SYSTEM/$DIR
+    #    lxc file push $OVERRIDE_BEFORE $INSTANCE/$ETC_SYSTEM/$DIR/override.conf
+    #done
+    #for DIR in $LOCAL_AFTER_D $NETWORK_AFTER_D $CONFIG_AFTER_D $FINAL_AFTER_D; do
+    #    lxc exec $INSTANCE -- mkdir -p /$ETC_SYSTEM/$DIR
+    #    lxc file push $OVERRIDE_AFTER $INSTANCE/$ETC_SYSTEM/$DIR/override.conf
+    #done
+    # override main
+    #lxc exec $INSTANCE -- mkdir -p /$LIB_SYSTEM/$MAIN_D
+    #printf '[Service]\nExecStart=\nExecStart=systemd-notify --ready --status "done"\n' > $OVERRIDE_MAIN
+    #lxc file push $OVERRIDE_MAIN $INSTANCE/$LIB_SYSTEM/$MAIN_D/override.conf
+
+    # re-run: no-op
+    clean_rerun $INSTANCE "divide-conquer-disabled" $GRAPHICAL
+
 }
 
 function gather_divide_conquer_enabled(){
@@ -234,11 +396,20 @@ function gather_divide_conquer_enabled(){
 
     # re-run: no-op
     clean_rerun $INSTANCE "divide-conquer-enabled" $CLOUD_INIT
-
-    # teardown
-    for DIR in /etc/systemd/system/cloud-init-local.service.d/ /etc/systemd/system/cloud-init-network.service.d/ /etc/systemd/system/cloud-config.service.d/ /etc/systemd/system/cloud-final.service.d/; do
-        lxc exec $INSTANCE -- rm -rf $DIR
+    for DIR in $LOCAL_D $NETWORK_D $CONFIG_D $FINAL_D; do
+        lxc exec $INSTANCE -- rm -rf /$ETC_SYSTEM/$DIR
     done
+    for DIR in $LOCAL_AFTER_D $NETWORK_AFTER_D $CONFIG_AFTER_D $FINAL_AFTER_D; do
+        lxc exec $INSTANCE -- rm -rf /$ETC_SYSTEM/$DIR
+    done
+    # delete "after services"
+    lxc exec $INSTANCE -- rm -f /$LIB_SYSTEM/cloud-init-local-after.service
+    lxc exec $INSTANCE -- rm -f /$LIB_SYSTEM/cloud-init-network-after.service
+    lxc exec $INSTANCE -- rm -f /$LIB_SYSTEM/cloud-config-after.service
+    lxc exec $INSTANCE -- rm -f /$LIB_SYSTEM/cloud-final-after.service
+
+    # override main
+    lxc exec $INSTANCE -- rm -rf /$LIB_SYSTEM/$MAIN_D
 }
 
 function gather_disabled(){
@@ -281,12 +452,13 @@ function run_test(){
 
     gather_first_boot $INSTANCE "$COMMAND"
 
-    #gather_divide_conquer_disabled $INSTANCE
-    #gather_divide_conquer_enabled $INSTANCE
     gather_no_ops $INSTANCE
     gather_disabled $INSTANCE
-    # gather_generator_no_op $INSTANCE
-    #gather_cached $INSTANCE
+    gather_generator_no_op $INSTANCE
+    gather_cached $INSTANCE
+    gather_divide_conquer_disabled $INSTANCE
+    gather_divide_conquer_enabled $INSTANCE
+    gather_tako_disabled $INSTANCE
 
     lxc rm -f $INSTANCE
 }
@@ -295,13 +467,15 @@ function run_test(){
 for ITER in $(seq 0 30); do
     mkdir -p $RESULTS/$ITER
     echo "running iteration: $ITER"
-    for INSTANCE in $SERIES $SERIES-vm; do
-        if [[ $INSTANCE == *"-vm" ]]; then
+    for TYPE in container; do #vm; do  # container; do
+        INSTANCE="$SERIES-$TYPE"
+        if [[ $INSTANCE == "vm" ]]; then
             COMMAND="$LAUNCH $INSTANCE --vm"
         else
             COMMAND="$LAUNCH $INSTANCE"
         fi
         lxc rm -f $INSTANCE 2>/dev/null || true
         run_test $INSTANCE "$COMMAND" $RESULTS/$ITER
+        exit 0
     done
 done
